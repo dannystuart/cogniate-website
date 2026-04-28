1510bddaeeb6e985ba5a244eafdb9cac1f71fefd
dannystuart <zsdan88@googlemail.com>
2026-04-28 14:51:30 +0100

    feat(design-system): add simple components to library
    Catalogue EyebrowBadge, ButtonPrimary (4 variants),
    and ButtonSecondary in the new Component Library section.

--- app/design-system/library/ComponentLibrary.tsx
+++ app/design-system/library/ComponentLibrary.tsx
@@ -1,3 +1,38 @@
+import EyebrowBadge from "../../components/EyebrowBadge";
+import ButtonPrimary from "../../components/ButtonPrimary";
+import ButtonSecondary from "../../components/ButtonSecondary";
+import LibraryCard from "./LibraryCard";
+import VariantTile from "./VariantTile";
+
 export default function ComponentLibrary() {
-  return <p className="text-text-secondary">Coming next</p>;
+  return (
+    <div className="flex flex-col gap-6">
+      <LibraryCard title="EyebrowBadge" importPath="app/components/EyebrowBadge.tsx">
+        <VariantTile label="Default" fullWidth>
+          <EyebrowBadge>Sample</EyebrowBadge>
+        </VariantTile>
+      </LibraryCard>
+
+      <LibraryCard title="ButtonPrimary" importPath="app/components/ButtonPrimary.tsx">
+        <VariantTile label="variant=white, size=default">
+          <ButtonPrimary variant="white" size="default">Click me</ButtonPrimary>
+        </VariantTile>
+        <VariantTile label="variant=white, size=small">
+          <ButtonPrimary variant="white" size="small">Click me</ButtonPrimary>
+        </VariantTile>
+        <VariantTile label="variant=coral, size=default">
+          <ButtonPrimary variant="coral" size="default">Click me</ButtonPrimary>
+        </VariantTile>
+        <VariantTile label="variant=coral, size=small">
+          <ButtonPrimary variant="coral" size="small">Click me</ButtonPrimary>
+        </VariantTile>
+      </LibraryCard>
+
+      <LibraryCard title="ButtonSecondary" importPath="app/components/ButtonSecondary.tsx">
+        <VariantTile label="Default" fullWidth>
+          <ButtonSecondary>Click me</ButtonSecondary>
+        </VariantTile>
+      </LibraryCard>
+    </div>
+  );
 }
